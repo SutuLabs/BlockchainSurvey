f05451594161d1d94c076e69e3acb5268fbcb72a
Icer <liangshuangde@163.com>
2021-03-26 20:42:59 +0800

    basic search

--- app.js
+++ app.js
@@ -1,43 +1,71 @@
 var app = new Vue({
     el: '#app',
-    data: {
-        papers,
-        columns: [{
-                field: 'title',
-                label: 'tt',
-                width: 100,
-                searchable: true,
-            },
-            {
-                field: 'year',
-                label: 'yy',
-                numeric: true,
-                width: 20,
-                searchable: true,
-            },
-            {
-                field: 'rank',
-                label: 'rk',
-                centered: true,
-                width: 20,
-                searchable: true,
-                options: [
-                    {value: "A", name:"A",},
-                    {value: "B", name:"B",},
-                    {value: "C", name:"C",},
-                    {value: "", name:"Any",},
-                    {value: "", name:"Any",},
-                ],
-            },
-            {
-                field: 'category',
-                label: 'c',
-                centered: true,
-                width: 20,
-                searchable: true,
-            },
-        ]
-    }
+    data: function () {
+        let generateOptions = function (array) {
+            return array.map(_ => ({
+                value: _,
+                name: _,
+            }));
+        };
+        let years = generateOptions(papers.map(_ => _.year).filter((val, idx, self) => self.indexOf(val) === idx).sort().reverse());
+        return {
+            papers,
+            ccfcats,
+            columns: [{
+                    field: 'title',
+                    label: 'Title',
+                    width: 100,
+                    searchable: true,
+                    search: function (a, input) {
+                        let ss = input.split(' ');
+                        for (let i = 0; i < ss.length; i++) {
+                            const str = ss[i];
+                            if (a.title.search(new RegExp(str, "i")) == -1)
+                                return false;
+                        }
+                        return true;
+                    },
+                },
+                {
+                    field: 'year',
+                    label: 'Year',
+                    centered: true,
+                    numeric: true,
+                    width: 20,
+                    searchable: true,
+                    options: years,
+                },
+                {
+                    field: 'rank',
+                    label: 'Rank',
+                    centered: true,
+                    width: 20,
+                    searchable: true,
+                    options: generateOptions(['Any', 'ABC', 'A', 'B', 'C', 'NotABC']),
+                    search: function (a, input) {
+                        if (input == "A" || input == "B" || input == "C") {
+                            if (a.rank == input) return true;
+                            return false;
+                        } else if (input == "Any") {
+                            return true;
+                        } else if (input == "NotABC") {
+                            return !a.rank;
+                        } else if (input == "ABC") {
+                            return a.rank ? true : false;
+                        }
+                    },
+                },
+                {
+                    field: 'category',
+                    label: 'Category',
+                    centered: true,
+                    width: 20,
+                    searchable: true,
+                },
+            ],
+        }
+    },
+    methods: {}
 })
 // const App = {
 //     data() {
