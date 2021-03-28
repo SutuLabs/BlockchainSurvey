e591460fb41bd905e6cf0f5c2b824ca213b2bfc6
Icer <liangshuangde@163.com>
2021-03-28 11:11:46 +0800

    take notes and compress

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ var app = new Vue({
             ccfcats,
             perPage: 10,
             isKeywordOpen: false,
+            isOutputOpen: false,
+            notes: {},
             columns: [{
                     field: 'title',
                     label: 'Title',
@@ -99,6 +101,28 @@ var app = new Vue({
             let col = this.columns.find(_ => _.field == 'title');
             Vue.set(col, 'filter', word);
         },
+        compress(c) {
+            var x = 'charCodeAt',
+                b, e = {},
+                f = c.split(""),
+                d = [],
+                a = f[0],
+                g = 256;
+            for (b = 1; b < f.length; b++) c = f[b], null != e[a + c] ? a += c : (d.push(1 < a.length ? e[a] : a[x](0)), e[a + c] = g, g++, a = c);
+            d.push(1 < a.length ? e[a] : a[x](0));
+            for (b = 0; b < d.length; b++) d[b] = String.fromCharCode(d[b]);
+            return d.join("")
+        },
+        decompress(b) {
+            var a, e = {},
+                d = b.split(""),
+                c = f = d[0],
+                g = [c],
+                h = o = 256;
+            for (b = 1; b < d.length; b++) a = d[b].charCodeAt(0), a = h > a ? d[b] : e[a] ? e[a] : f + c, g.push(a), c = a.charAt(0), e[o] = f + c, o++, f = a;
+            return g.join("")
+        },
+
     },
     computed: {
         filterPapers() {
